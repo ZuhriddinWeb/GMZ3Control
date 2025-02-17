02243e1c578842a054740b756be01e23f49e3e0b
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2025-02-17 11:30:26 +0500

    dd commented

--- app/Observers/ValuesParametersObserver.php
+++ app/Observers/ValuesParametersObserver.php
@@ -143,7 +143,7 @@ class ValuesParametersObserver
 
                     // Tekshirish: Natija bazaga to'g'ri yozilganligini ko'rish uchun
                     logger()->info("Bazaga yozilgan yozuv: ", $newOrUpdateRecord->toArray());
-                    dd($newOrUpdateRecord); // Agar kerak bo'lsa, bu qator natijani tekshirish uchun ishlatilishi mumkin
+                    // dd($newOrUpdateRecord); // Agar kerak bo'lsa, bu qator natijani tekshirish uchun ishlatilishi mumkin
                 });
             }
         });

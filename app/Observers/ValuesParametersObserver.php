3128c661a20edf9ea54eeb4d5346bd7a51f7685a
Zuhriddin <zuhriddin_mustafoyev@mail.ru>
2025-03-12 10:39:54 +0500

    id changed

--- app/Observers/ValuesParametersObserver.php
+++ app/Observers/ValuesParametersObserver.php
@@ -78,18 +78,21 @@ class ValuesParametersObserver
                     } elseif (strpos($item, 'Tid=') === 0) {
                         $timeId = substr($item, 4);
 
-                        // $graphicTimeName = DB::table('graphic_times')->where('TimeID', $timeId)->value('Name');
-                        // dd($graphicTimeName);
-                        
-                        // Har bir unikal Tid uchun parametr qiymatini olish
+                        $graphicTimeName = DB::table('graphic_times')
+                            ->where('id', $timeId)
+                            ->value('Name');
+
+                        $relatedTimeIds = DB::table('graphic_times')
+                            ->where('Name', $graphicTimeName)
+                            ->pluck('id');
+
                         $parameters[$parameterId][$timeId] = $parameters[$parameterId][$timeId] ?? 
                             ValuesParameters::where('ParametersID', $parameterId)
-                            ->where('TimeID', $timeId)
-                            ->where('Created',$valuesParameters->Created)
-                            ->value('Value') ?? 0;
+                                ->whereIn('TimeID', $relatedTimeIds)
+                                ->where('Created', $valuesParameters->Created)
+                                ->value('Value') ?? 0;
                     }
                 }
-
                 // Hisoblash ifodasini yaratish uchun `calculateArray` ichidagi har bir elementni ko‘rib chiqish
                 foreach ($calculateArray as $item) {
                     if (strpos($item, 'Pid=') === 0) {
